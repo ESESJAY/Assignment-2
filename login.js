7f98b59ba0202326245574b8a5682c3ff2df4e64
Soo Shan Jing <s10220988@connect.np.edu.sg>
2023-02-06 23:19:15 +0800

    login page

--- login.js
+++ login.js
@@ -8,3 +8,60 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+$(document).ready(function(){
+	$("button").click(function(){
+	  $.ajax({
+		type: 'POST',
+		url: "https://assignment2-2816.restdb.io/rest/contact",
+		headers: {
+		  'Content-Type': 'application/json',
+		  'x-apikey': '63e1075d3bc6b255ed0c46fe'
+		},
+		data: JSON.stringify({
+		  name: $("input[name='name']").val(),
+		  email: $("input[name='email']").val(),
+		  password: $("input[name='password']").val()
+		}),
+		success: function(data) {
+		  console.log(data);
+		}
+	  });
+	});
+  });
+
+  
+  var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://assignment2-2816.restdb.io/rest/contact",
+	"method": "GET",
+	"headers": {
+	"content-type": "application/json",
+	"x-apikey": "63e1075d3bc6b255ed0c46fe",
+	"cache-control": "no-cache"
+	}
+  }
+
+
+	var found = false;
+    $(document).ready(function() {
+	$("button").click(function() {
+	var email = $("input[name='email']").val();
+	var password = $("input[name='password']").val();
+  	$.ajax(settings).done(function (response) {
+		for (var i = 0; i < response.length; i++) {
+		if (JSON.stringify(response[i].email) == JSON.stringify(email) && JSON.stringify(response[i].password)== JSON.stringify(password)){
+			found = true;
+			break;
+	  }
+	}
+	if (found) {
+	  alert("Welcome back!");
+	  window.location.href = "index.html";
+	} else {
+	  alert("Wrong username or password!");
+	}
+  });
+  });
+  }); 
