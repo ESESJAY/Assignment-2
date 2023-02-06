58ccabac0d7e93d5bd252ce6cb0e6f052e8b2dd6
KShengYao <s10247882@connect.np.edu.sg>
2023-02-07 03:28:54 +0800

    updated

--- login.js
+++ login.js
@@ -9,73 +9,69 @@ signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
 
-$(document).ready(function(){
-	const APIKEY = "63e1075d3bc6b255ed0c46fe";
-	$("#Button-SignUp").click(function(){
-		let signUpName = $("#nameSignUp").val();
-		let signUpEmail = $("#emailSignUp").val();
-		let signUpPassword = $("#passwordSignUp").val();
+$(document).ready(function() {
+	const APIKEY = "63e1500c3bc6b255ed0c471e";
+	$("#Button-SignUp").on("click", function () {
 
-		let jsondata = {
-			"name": signUpName,
-			"email": signUpEmail,
-			"password": signUpPassword,
-		};
-		console.log(jsondata)
+	  let name = $("#nameSignUp").val();
+	  let email = $("#emailSignUp").val();
+	  let password = $("#passwordSignUp").val();
 
-		let settings = {
-			"async": true,
-			"crossDomain": true,
-			"url": "https://assignment2-2816.restdb.io/rest/contact",
-			"method": "POST", 
-			"headers": {
-				"content-type": "application/json",
-				"x-apikey": APIKEY,
-				"cache-control": "no-cache"
-			},
+	  let jsondata = {"name": name,"email": email,"password": password};
+	  var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://assignment2-2816.restdb.io/rest/contact",
+		"method": "POST",
+		"headers": {
+		  "content-type": "application/json",
+		  "x-apikey": APIKEY,
+		  "cache-control": "no-cache"
+		},
 		"processData": false,
-		"data": JSON.stringify(jsondata),
-		}
-
-		$.ajax(settings).done(function (response) {
-			console.log(response);
-		});
+		"data": JSON.stringify(jsondata)
+	  }
+	  
+	  console.log(settings.data)
+	  $.ajax(settings).done(function (response) {
+		console.log(response);
+	  });
 	});
-  });
+});
 
   
-  var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://assignment2-2816.restdb.io/rest/contact",
-	"method": "GET",
-	"headers": {
-	  "content-type": "application/json",
-	  "x-apikey": "63e1075d3bc6b255ed0c46fe",
-	  "cache-control": "no-cache"
-	}
-  }
+//   var settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://assignment2-2816.restdb.io/rest/contact",
+// 	"method": "GET",
+// 	"headers": {
+// 	  "content-type": "application/json",
+// 	  "x-apikey": "63e1500c3bc6b255ed0c471e",
+// 	  "cache-control": "no-cache"
+// 	}
+//   }
 
 
 
-  $(document).ready(function() {
-	$("ButtonSignIn").click(function() {
-	var email = $("input[name='email']").val();
-	var password = $("input[name='password']").val();
-  $.ajax(settings).done(function (response) {
-	var found = false;
-	for (var i = 0; i < response.length; i++) {
-	  if (JSON.stringify(response[i].email) == JSON.stringify(email) && JSON.stringify(response[i].password)== JSON.stringify(password)) {
-		found = true;
-		break;
-	  }
-	}
-	if (found) {
-	  alert("Welcome back!");
-	  window.location.href = "index.html";
-	} else {
-	  alert("Wrong username or password!");
-	}
-  });
-  });
-  });
+//   $(document).ready(function() {
+// 	$("ButtonSignIn").click(function() {
+// 	var email = $("input[name='email']").val();
+// 	var password = $("input[name='password']").val();
+//   $.ajax(settings).done(function (response) {
+// 	var found = false;
+// 	for (var i = 0; i < response.length; i++) {
+// 	  if (JSON.stringify(response[i].email) == JSON.stringify(email) && JSON.stringify(response[i].password)== JSON.stringify(password)) {
+// 		found = true;
+// 		break;
+// 	  }
+// 	}
+// 	if (found) {
+// 	  alert("Welcome back!");
+// 	  window.location.href = "index.html";
+// 	} else {
+// 	  alert("Wrong username or password!");
+// 	}
+//   });
+//   });
+//   });
